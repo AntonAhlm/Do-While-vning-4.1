34c2cc0a34409001525263454abe6e8f34b54a93
Anton <>
2023-02-17 09:01:01 +0100

    Add project files.

---
+++ Do-While Övning 4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många kvadrattal ska skrivas ut?");
+            int Tal = Convert.ToInt32(Console.ReadLine());
+            int Tal1 = 1;
+            do
+            {
+                Console.WriteLine(Tal1+"*"+Tal1+"="+(Tal1*Tal1));
+                Tal1++;
+            } while (Tal1<=Tal);
+
+        }
+    }
+}
